abcdf0490e02c9231b90f23545a01a3ae72509a7
fdzrn <fadzriangibran@gmail.com>
2019-08-01 10:11:45 +0700

    Tugas 19 javascript
    untuk niomic

---
+++ tugas19.js
@@ -0,0 +1,17 @@
+function tugas19Satu() {
+	console.log('Semangat...'.repeat(5));
+}
+tugas19Satu();
+console.log("\n");
+function tugas19Dua() {
+	console.log("*".repeat(5));
+	console.log("*".repeat(20));
+	console.log("*".repeat(40));
+	console.log("*".repeat(60));
+	console.log("*".repeat(80));
+	console.log("*".repeat(60));
+	console.log("*".repeat(40));
+	console.log("*".repeat(20));
+	console.log("*".repeat(5));
+}
+tugas19Dua();
